f7d43d07f14432af397cc17479caf74e22c05146
Aarthika-R <aarthika.r.2023.vlsi@ritchennai.edu.in>
2026-04-11 20:28:01 +0530

    Refactor full subtractor implementation with behavioral, dataflow, and gate-level modeling. Update testbench to include comprehensive monitoring for each model.

--- subractor/full_sub/full.v
+++ subractor/full_sub/full.v
@@ -1,4 +1,4 @@
-module full (
+module full_behavioural (
     a,b,c,diff,bor
 );
     input a,b,c;
@@ -8,4 +8,30 @@ module full (
         diff = a^b^c;
         bor = ~a&b|~(a^b)&c;
     end
+endmodule
+
+module full_dataflow (
+    a,b,c,diff,bor
+);
+    input a,b,c;
+    output diff,bor;
+
+    assign diff = a^b^c;
+    assign bor = (~a&b)|~(a^b)&c;
+endmodule
+
+module full_gatelevel (
+    a,b,c,diff,bor
+);
+    input a,b,c;
+    output diff,bor;
+    wire t1,t2,t3,t4;
+
+    xor g1(t1,a,b);
+    xor g2(diff,t1,c);
+    not g3(t2,a);
+    and g4(t3,t2,b);
+    not g5(t4,t1);
+    and g6(t5,t4,c);
+    or g7(bor,t3,t5);
 endmodule
--- subractor/full_sub/full_tb.v
+++ subractor/full_sub/full_tb.v
@@ -1,23 +1,42 @@
 module full_tb;
-    reg a,b,c;
-    wire diff,bor;
+    reg a_b,b_b,c_b,a_d,b_d,c_d,a_g,b_g,c_g;
+    wire diff_b,bor_b,diff_d,bor_d,diff_g,bor_g;
 
-    full dut(.a(a),.b(b),.c(c),.diff(diff),.bor(bor));
+    full_behavioural dut_b(.a(a_b),.b(b_b),.c(c_b),.diff(diff_b),.bor(bor_b));
+    full_dataflow dut_d(.a(a_d),.b(b_d),.c(c_d),.diff(diff_d),.bor(bor_d));
+    full_gatelevel dut_g(.a(a_g),.b(b_g),.c(c_g),.diff(diff_g),.bor(bor_g));
 
     initial begin
         $dumpfile("full_output.vcd");
         $dumpvars(0,full_tb);
-        $monitor("a=%b  b=%b  c=%b  diff=%b   bor=%b", a,b,c,diff,bor);
+        $monitor("behavioural: a=%b  b=%b  c=%b  diff=%b   bor=%b", a_b,b_b,c_b,diff_b,bor_b);
+        a_b=0; b_b=0; c_b=0; #10; 
+        a_b=0; b_b=0; c_b=1; #10;   
+        a_b=0; b_b=1; c_b=0; #10;   
+        a_b=0; b_b=1; c_b=1; #10;    
+        a_b=1; b_b=0; c_b=0; #10;  
+        a_b=1; b_b=0; c_b=1; #10;   
+        a_d=1; b_d=1; c_d=0; #10;   
+        a_d=1; b_d=1; c_d=1; #10; 
 
-        a=0; b=0; c=0; #10; 
-        a=0; b=0; c=1; #10;   
-        a=0; b=1; c=0; #10;   
-        a=0; b=1; c=1; #10;    
-        a=1; b=0; c=0; #10;  
-        a=1; b=0; c=1; #10;   
-        a=1; b=1; c=0; #10;   
-        a=1; b=1; c=1; #10; 
-
+        $monitor("dataflow: a=%b  b=%b  c=%b  diff=%b   bor=%b", a_d,b_d,c_d,diff_d,bor_d);
+        a_d=0; b_d=0; c_d=0; #10;
+        a_d=0; b_d=0; c_d=1; #10;
+        a_d=0; b_d=1; c_d=0; #10;
+        a_d=0; b_d=1; c_d=1; #10;
+        a_d=1; b_d=0; c_d=0; #10;
+        a_d=1; b_d=0; c_d=1; #10;
+        a_d=1; b_d=1; c_d=0; #10;
+        a_d=1; b_d=1; c_d=1; #10;
+        $monitor("gatelevel: a=%b  b=%b  c=%b  diff=%b   bor=%b", a_g,b_g,c_g,diff_g,bor_g);
+        a_g=0; b_g=0; c_g=0; #10;
+        a_g=0; b_g=0; c_g=1; #10;
+        a_g=0; b_g=1; c_g=0; #10;
+        a_g=0; b_g=1; c_g=1; #10;
+        a_g=1; b_g=0; c_g=0; #10;
+        a_g=1; b_g=0; c_g=1; #10;
+        a_g=1; b_g=1; c_g=0; #10;
+        a_g=1; b_g=1; c_g=1; #10;
         $finish;
     end
 endmodule
