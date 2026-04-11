7e123e788dc3438e7de65bf7ac6a2d171cc9a433
Aarthika-R <aarthika.r.2023.vlsi@ritchennai.edu.in>
2026-04-11 16:34:44 +0530

    AND by Universal Gates

--- gates_by_universal_gates/and/and.v
+++ gates_by_universal_gates/and/and.v
@@ -18,37 +18,3 @@ module and_by_nor (
     nor_gate nor2(.a(b),.b(b),.y(t2));
     nor_gate nor3(.a(t1),.b(t2),.y(y));
 endmodule
-
-// module nor_gate (
-//     output y,input a,b
-// );
-//     supply1 vdd;
-//     supply0 gnd;
-//     wire p;
-//     //series pmos(drain,soure,gate)
-//     pmos (p,vdd,a);
-//     pmos (y,p,b);
-
-//     //parallel nmos(drain source gate)
-
-//     nmos(y,gnd,a);
-//     nmos(y,gnd,b);
-
-// endmodule
-
-
-// module nand_gate(output y, input a,b);
-//     supply1 vdd;
-//     supply0 gnd;
-    
-//     wire n;
-//     //pmos(darin,source,gate)
-//     pmos s1(y,vdd,a);
-//     pmos s2(y,vdd,b);
-
-//     //nmos(drain,source,gate)
-//     nmos(n,gnd,b);
-//     nmos(y,n,a);
-    
-    
-// endmodule
