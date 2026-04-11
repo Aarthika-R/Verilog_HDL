93495fd4a9d87b025bcc8cac5d5a96777af45fe3
Aarthika-R <aarthika.r.2023.vlsi@ritchennai.edu.in>
2026-04-11 19:54:16 +0530

    Refactor full adder implementation with different modeling and testbench

--- adder/full_adder/full_adder.v
+++ adder/full_adder/full_adder.v
@@ -1,4 +1,4 @@
-module full_adder (
+module full_adder_dataflow(
     input a,
     input b,
     input c,
@@ -8,4 +8,29 @@ module full_adder (
    
     assign y = a^b^c;
     assign Cout = (a&b)|(a&c)|(b&c);
+endmodule
+
+module full_adder_behavioural (
+    sum,carry,a,b,c
+);
+    input a,b,c;
+    output reg sum,carry;
+    always @(*) begin
+        sum=a^b^c;
+        carry=(a&b)|(a&c)|(b&c);
+    end
+endmodule
+
+module full_adder_gatelevel (
+    output sum,carry,input a,b,c
+);
+    wire t1,t2,t3,t4,t5;
+    xor g1(t1,a,b);
+    xor g2(sum,t1,c);
+    and g3(t2,a,b);
+    and g4(t3,b,c);
+    and g5(t4,a,c);
+    or g6(t5,t2,t3);
+    or g7(carry,t4,t5);
+
 endmodule
--- adder/full_adder/full_adder_tb.v
+++ adder/full_adder/full_adder_tb.v
@@ -1,28 +1,66 @@
 module full_adder_tb();
-    reg a,b,c;
-    wire Cout,y;
+    reg a_d,b_d,c_d,a_b,b_b,c_b,a_g,b_g,c_g;
+    wire Cout_d,y_d,Cout_b,sum_b,Cout_g,sum_g;
 
-    full_adder uut(
-        .a(a),
-        .b(b),
-        .c(c),
-        .y(y),
-        .Cout(Cout)
+    full_adder_dataflow uut_d(
+        .a(a_d),
+        .b(b_d),
+        .c(c_d),
+        .y(y_d),
+        .Cout(Cout_d)
+    
+    );
+    full_adder_behavioural uut_b(
+        .a(a_b),
+        .b(b_b),
+        .c(c_b),
+        .sum(sum_b),
+        .carry(Cout_b)
+    
+    );
+    full_adder_gatelevel uut_g(
+        .a(a_g),
+        .b(b_g),
+        .c(c_g),
+        .sum(sum_g),
+        .carry(Cout_g)
     
     );
     initial begin
 
         $dumpfile("wave.vcd");   
         $dumpvars(0, full_adder_tb);
-        $monitor("Time=%0t | a=%b b=%b c=%b -> y=%b Cout=%b", $time, a, b, c, y, Cout);
-        a=0; b=0; c=0; #10; 
-        a=0; b=0; c=1; #10;   
-        a=0; b=1; c=0; #10;   
-        a=0; b=1; c=1; #10;    
-        a=1; b=0; c=0; #10;  
-        a=1; b=0; c=1; #10;   
-        a=1; b=1; c=0; #10;   
-        a=1; b=1; c=1; #10;   
+        $monitor("dataflow  Time=%0t | a=%b b=%b c=%b -> y=%b Cout=%b", $time, a_d, b_d, c_d, y_d, Cout_d);
+        a_d=0; b_d=0; c_d=0; #10; 
+        a_d=0; b_d=0; c_d=1; #10;   
+        a_d=0; b_d=1; c_d=0; #10;   
+        a_d=0; b_d=1; c_d=1; #10;    
+        a_d=1; b_d=0; c_d=0; #10;  
+        a_d=1; b_d=0; c_d=1; #10;   
+        a_d=1; b_d=1; c_d=0; #10;   
+        a_d=1; b_d=1; c_d=1; #10;   
+
+
+        $monitor("behavioural  Time=%0t | a=%b b=%b c=%b -> sum=%b carry=%b", $time, a_b, b_b, c_b, sum_b, Cout_b);
+        a_b=0; b_b=0; c_b=0; #10;
+        a_b=0; b_b=0; c_b=1; #10;
+        a_b=0; b_b=1; c_b=0; #10;
+        a_b=0; b_b=1; c_b=1; #10;
+        a_b=1; b_b=0; c_b=0; #10;
+        a_b=1; b_b=0; c_b=1; #10;
+        a_b=1; b_b=1; c_b=0; #10;
+        a_b=1; b_b=1; c_b=1; #10;
+        
+        $monitor("gatelevel  Time=%0t | a=%b b=%b c=%b -> sum=%b carry=%b", $time, a_g, b_g, c_g, sum_g, Cout_g);
+        a_g=0; b_g=0; c_g=0; #10;
+        a_g=0; b_g=0; c_g=1; #10;
+        a_g=0; b_g=1; c_g=0; #10;
+        a_g=0; b_g=1; c_g=1; #10;
+        a_g=1; b_g=0; c_g=0; #10;
+        a_g=1; b_g=0; c_g=1; #10;
+        a_g=1; b_g=1; c_g=0; #10;
+        a_g=1; b_g=1; c_g=1; #10;
+        
 
         $finish;
     end
