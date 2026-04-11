862aa746ba45ef4c29ee25de6928432f40e5f1a6
Aarthika-R <aarthika.r.2023.vlsi@ritchennai.edu.in>
2026-04-11 20:06:37 +0530

    Add half adder implementations with dataflow, behavioral, and gate-level modeling, including testbench updates

--- subractor/half_sub/half.v
+++ subractor/half_sub/half.v
@@ -1,4 +1,4 @@
-module half (
+module half_dataflow(
     a,b,diff,bor
 );
     input a,b;
@@ -6,4 +6,27 @@ module half (
 
     assign diff = a^b;
     assign bor = ~a&b;
+endmodule
+
+module half_behavioural (
+    a,b,diff,bor
+);
+    input a,b;
+    output reg diff,bor;
+    always @(*) begin
+        diff = a^b;
+        bor = ~a&b;
+    end
+endmodule
+
+module half_gatelevel (
+    a,b,diff,bor
+);
+    input a,b;
+    output diff,bor;
+    wire n;
+
+    xor g1(diff,a,b);
+    not g2(n,a);
+    and g3(bor,n,b);
 endmodule
--- subractor/half_sub/half_tb.v
+++ subractor/half_sub/half_tb.v
@@ -1,20 +1,32 @@
 module half_tb;
-    reg a,b;
-    wire  bor,diff;
+    reg a_b,b_b,a_g,b_g,a_d,b_d;
+    wire  bor_b,diff_b,bor_g,diff_g,bor_d,diff_d;
 
-    half dut(.a(a),.b(b),.diff(diff),.bor(bor));
+    half_dataflow dut_dataflow(.a(a_b),.b(b_b),.diff(diff_b),.bor(bor_b));
+    half_behavioural dut_behavioural(.a(a_g),.b(b_g),.diff(diff_g),.bor(bor_g));
+    half_gatelevel dut_gatelevel(.a(a_d),.b(b_d),.diff(diff_d),.bor(bor_d));
 
     initial begin
         $dumpfile("half_output.vcd");
         $dumpvars(0,half_tb);
 
-        $monitor("a=%b   b=%b   diff=%b   borrow=%b", a,b,diff,bor);
+        $monitor("behavioural: a=%b   b=%b   diff=%b   borrow=%b", a_b,b_b,diff_b,bor_b);
+        a_b=0; b_b=0; #10;
+        a_b=0; b_b=1; #10;
+        a_b=1; b_b=0; #10;
+        a_b=1; b_b=1; #10;
 
-        a=0; b=0; #10;
-        a=0; b=1; #10;
-        a=1; b=0; #10;
-        a=1; b=1; #10;
+        $monitor("gate-level: a=%b   b=%b   diff=%b   borrow=%b", a_g,b_g,diff_g,bor_g);
+        a_g=0; b_g=0; #10;
+        a_g=0; b_g=1; #10;
+        a_g=1; b_g=0; #10;
+        a_g=1; b_g=1; #10;
 
+        $monitor("dataflow: a=%b   b=%b   diff=%b   borrow=%b", a_d,b_d,diff_d,bor_d);
+        a_d=0; b_d=0; #10;
+        a_d=0; b_d=1; #10;
+        a_d=1; b_d=0; #10;
+        a_d=1; b_d=1; #10;
         $finish;
     end
 endmodule
